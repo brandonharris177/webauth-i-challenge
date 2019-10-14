fedcff11c20f8cf6828c616dbd8353472233cf53
Brandon-Harris <brandonharris177@gmail.com>
2019-10-14 14:51:06 -0600

    migration set

--- users/users-model.js
+++ users/users-model.js
@@ -3,16 +3,11 @@ const db = require('../data/dbConfig.js');
 module.exports = {
   add,
   find,
-  findBy,
   findById,
 };
 
 function find() {
   return db('users').select('id', 'username', 'password');
-}
-
-function findBy(filter) {
-  return db('users').where(filter);
 }
 
 function add(user) {
--- users/users-router.js
+++ users/users-router.js
@@ -5,7 +5,49 @@ const Users = require('./users-model');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-res.json('working from router')
+    Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Failed to get users' });
+    });
 });
+
+server.post('/register', (req, res) => {
+    let user = req.body;
+    // validate the user
+    // hash the password
+    const hash = bcrypt.hashSync(user.password, 8);
+    // we override the password with the hash
+    user.password = hash;
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+
+  server.post('/api/login', (req, res) => {
+    let { username, password } = req.body;
+    if (username && password) {
+      Users.findBy({ username })
+        .first()
+        .then(user => {
+          if (user && bcrypt.compareSync(password, user.password)) {
+            res.status(200).json({ message: `Welcome ${user.username}!` });
+          } else {
+            res.status(401).json({ message: 'You cannot pass!!' });
+          }
+        })
+        .catch(error => {
+          res.status(500).json(error);
+        });
+    } else {
+      res.status(400).json({ message: 'please provide credentials' });
+    }
+  });
 
 module.exports = router;
