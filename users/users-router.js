3d380d96da518c1355c19dfa4ff04d2a5ec17c99
Brandon-Harris <brandonharris177@gmail.com>
2019-10-15 13:46:50 -0600

    day 2 MVP

--- users/users-router.js
+++ users/users-router.js
@@ -4,22 +4,13 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
 const Users = require('../users/users-model.js');
+const restricted = require('./restriced-middleware');
 
 const router = express();
 
 router.use(helmet());
 router.use(express.json());
 router.use(cors());
-
-router.get('/', (req, res) => {
-    Users.find()
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'Failed to get users' });
-    });
-});
 
 router.post('/register', (req, res) => {
     let user = req.body;
@@ -42,33 +33,37 @@ router.post('/register', (req, res) => {
   });
   
 router.post('/login', (req, res) => {
-let { username, password } = req.body;
-if (username && password) {
-    Users.findBy({ username })
+  let { username, password } = req.body;
+
+  // console.log('session', req.session);
+
+  Users.findBy({ username })
     .first()
     .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}! you are logged in` });
-        } else {
-        res.status(401).json({ message: 'You cannot pass!!' });
-        }
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.username = user.username;
+
+        // console.log('session', req.session);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
     })
     .catch(error => {
-        res.status(500).json(error);
+      res.status(500).json(error);
     });
-} else {
-    res.status(400).json({ message: 'please provide credentials' });
-}
 });
+  
 
-router.get('/users', (req, res) => {
-    Users.find()
+router.get('/users', restricted, (req, res) => {
+  console.log('username', req.session.username);
+  Users.find()
     .then(users => {
       res.json(users);
     })
-    .catch(err => {
-      res.status(500).json({ message: 'Failed to get users' });
-    });
+    .catch(err => res.send(err));
 });
 
 module.exports = router;
